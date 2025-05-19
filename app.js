3c3dcd066afdc22448b14160e48778c42204305a
KanishkSingh0801 <kanishksinghjatav@gmail.com>
2025-05-19 14:04:49 +0530

    Test entry in DB

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/test";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
@@ -11,9 +12,10 @@ main()
   .catch((err) => {
     console.log(err);
   });
+
 async function main() {
   mongoose.connect(MONGO_URL);
-}
+};
 
 app.listen(8080, () => {
   console.log("Server is listening to port 8080");
@@ -21,4 +23,17 @@ app.listen(8080, () => {
 
 app.get("/", (req, res) => {
   res.send("Hi, i am root");
+});
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title : "New villa",
+        description : "By the beach",
+        price : 1200,
+        location : "Goa",
+        country : "India"
+    });
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("Testing successful");
 });
